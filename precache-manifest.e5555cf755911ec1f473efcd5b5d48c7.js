7f728a2733cbf215a32d7e6c25f611f5d07baf30
turchak <turchaksv@gmail.com>
2019-02-20 03:00:23 +0200

    Updates

---
+++ precache-manifest.e5555cf755911ec1f473efcd5b5d48c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "650ab8a2a76bcde5f85b",
+    "url": "/react-barcode/static/js/runtime~main.650ab8a2.js"
+  },
+  {
+    "revision": "5049832ccbe738603d3e",
+    "url": "/react-barcode/static/js/main.5049832c.chunk.js"
+  },
+  {
+    "revision": "137ecc98fb3bf0078858",
+    "url": "/react-barcode/static/js/2.137ecc98.chunk.js"
+  },
+  {
+    "revision": "5049832ccbe738603d3e",
+    "url": "/react-barcode/static/css/main.a0f8a827.chunk.css"
+  },
+  {
+    "revision": "6eecd5e979594a8cf5721548aeb4bbf4",
+    "url": "/react-barcode/index.html"
+  }
+];
