3855d81362f2f25c6fe9066e123bcef3d8263305
turchak <turchaksv@gmail.com>
2019-02-21 20:08:34 +0200

    Add node sass

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import ScannerContainer from './containers/ScannerContainer';
-import './index.css';
+import './index.sass';
 
 ReactDOM.render(<ScannerContainer />, document.getElementById('root'));
 
